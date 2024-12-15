3503ee7ce42191e0277fd918587a667ee5b7df05
fikinaaaa <fikisentlong.com>
2024-12-16 00:21:26 +0700

    rasyd
    penyakit,gejala, ui semua admin done

---
+++ partial/footerA.php
@@ -0,0 +1,20 @@
+  <!--footer-->
+ <!-- footerA.php -->
+<footer id="footer" style="background-color: #000; color: #fff; padding: 20px 0; text-align: center; position: fixed; bottom: 0; left: 0; width: 100%;">
+    <div class="container">
+        <p>© Copyright Peduli Cabai. All Rights Reserved</p>
+        <div class="credits">
+            Designed by <a href="#" style="color: #0cb8b6;">BabangTamvvan</a>
+        </div>
+    </div>
+</footer>
+  <!--/ footer-->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
