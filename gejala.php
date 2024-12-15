303826d3402118ed48217c88030dfab5b3702498
fikinaaaa <fikisentlong.com>
2024-12-15 23:05:41 +0700

    admin
    gejala

---
+++ gejala.php
@@ -0,0 +1,56 @@
+<?php include("partial/header.php");?>
+  <!--service-->
+  <section id="service" class="section-padding">
+  <?php include("partial/navAdmin.php");?>
+    <div class="container" style="margin-top: 50px;">
+      <div class="row">
+        <div class="col-md-4 col-sm-4">
+          <h2 class="ser-title">Our Service</h2>
+          <hr class="botm-line">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris cillum.</p>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="service-info">
+            <div class="icon">
+              <i class="fa fa-stethoscope"></i>
+            </div>
+            <div class="icon-info">
+              <h4>24 Hour Support</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+          <div class="service-info">
+            <div class="icon">
+              <i class="fa fa-ambulance"></i>
+            </div>
+            <div class="icon-info">
+              <h4>Emergency Services</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="service-info">
+            <div class="icon">
+              <i class="fa fa-user-md"></i>
+            </div>
+            <div class="icon-info">
+              <h4>Medical Counseling</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+          <div class="service-info">
+            <div class="icon">
+              <i class="fa fa-medkit"></i>
+            </div>
+            <div class="icon-info">
+              <h4>Premium Healthcare</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ service-->
+<?php include("partial/footer.php");?>
---
+++ partial/navAdmin.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="col-md-12">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/<?= explode('/', $_SERVER['REQUEST_URI'])[1] ?>/index.php" class="me-2 <?php echo ($path == 'index.php' ? 'active' : '') ?>">
+          <h2 class="title"><span style="color: green;">peduli</span><span style="color: red;">cabai</span></h2>
+        </a>
+      </div>
+      <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li><a href="/<?= explode('/', $_SERVER['REQUEST_URI'])[1] ?>/index.php" class="me-2 <?php echo ($path == 'index.php' ? 'active' : '') ?>">Gejala</a></li>
+          <li><a href="/<?= explode('/', $_SERVER['REQUEST_URI'])[1] ?>/diagnosa.php" class="me-2 <?php echo ($path == 'diagnosa.php' ? 'active' : '') ?>">Penyakit</a></li>
+          <li><a href="/<?= explode('/', $_SERVER['REQUEST_URI'])[1] ?>/login.php" class="me-2 <?php echo ($path == 'login.php' ? 'active' : '') ?>">Log Out</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
